19aa1fd62f6fc04332b5aca4840d045d6b8bcc53
hoonho <hoonho0510@gmail.com>
2024-11-18 17:32:30 +0900

    이메일, 동의 제거

--- js/signup.js
+++ js/signup.js
@@ -27,21 +27,18 @@
 	});
 
 	// 폼 제출 처리
-	$("#signup-button").click(function(e) {
+	$("#signup-form").submit(function(e) {
 	    e.preventDefault();
 	    
 	    var username = $("#username").val();
 	    var password = $("#password").val();
 	    var confirmPassword = $("#password-confirm").val();
-	    var email = $("#email").val();
-	    var agreeTerms = $("#agree-terms").is(":checked");
 	    
 	    var emptyFields = [];
 	    
 	    if (!username) emptyFields.push("아이디");
 	    if (!password) emptyFields.push("비밀번호");
 	    if (!confirmPassword) emptyFields.push("비밀번호 확인");
-	    if (!email) emptyFields.push("이메일");
 	    
 	    if (emptyFields.length > 0) {
 	        alert("다음 필드를 입력해주세요: " + emptyFields.join(", "));
@@ -50,11 +47,6 @@
 	    
 	    if (password !== confirmPassword) {
 	        alert("비밀번호가 일치하지 않습니다.");
-	        return;
-	    }
-	    
-	    if (!agreeTerms) {
-	        alert("서비스 이용약관에 동의해주세요.");
 	        return;
 	    }
 	    
